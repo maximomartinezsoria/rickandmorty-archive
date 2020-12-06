ef6af1782ec5a2652e987f93fda4c1e561f2508c
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2020-12-06 13:16:25 -0300

    Characters mock

---
+++ src/frontend/__mocks__/CharactersMock.js
@@ -0,0 +1,53 @@
+export default [
+  {
+    id: 1,
+    name: 'Rick Sanchez',
+    status: 'Alive',
+    species: 'Human',
+    type: '',
+    gender: 'Male',
+    origin: {
+      name: 'Earth (C-137)',
+      url: 'https://rickandmortyapi.com/api/location/1',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+      url: 'https://rickandmortyapi.com/api/location/20',
+    },
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+  },
+  {
+    id: 2,
+    name: 'Morty Smith',
+    status: 'Alive',
+    species: 'Human',
+    type: '',
+    gender: 'Male',
+    origin: {
+      name: 'Earth (C-137)',
+      url: 'https://rickandmortyapi.com/api/location/1',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+      url: 'https://rickandmortyapi.com/api/location/20',
+    },
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+  },
+  {
+    id: 3,
+    name: 'Summer Smith',
+    status: 'Alive',
+    species: 'Human',
+    type: '',
+    gender: 'Female',
+    origin: {
+      name: 'Earth (Replacement Dimension)',
+      url: 'https://rickandmortyapi.com/api/location/20',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+      url: 'https://rickandmortyapi.com/api/location/20',
+    },
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+  },
+]
--- src/frontend/__mocks__/InitialStateMock.js
+++ src/frontend/__mocks__/InitialStateMock.js
@@ -1,24 +1,8 @@
+import charactersMock from './CharactersMock'
+
 const initialStateMock = {
   characters: {
-    characters: [
-      {
-        id: 1,
-        name: 'Rick Sanchez',
-        status: 'Alive',
-        species: 'Human',
-        type: '',
-        gender: 'Male',
-        origin: {
-          name: 'Earth (C-137)',
-          url: 'https://rickandmortyapi.com/api/location/1',
-        },
-        location: {
-          name: 'Earth (Replacement Dimension)',
-          url: 'https://rickandmortyapi.com/api/location/20',
-        },
-        image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-      },
-    ],
+    characters: charactersMock,
     nameQuery: 'rick',
     statusQuery: 'Dead',
   },
