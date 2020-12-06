3651c4b74f4329fafd4565d28045f9df04353312
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2020-12-06 13:14:30 -0300

    InitialState Mock

---
+++ src/frontend/__mocks__/InitialStateMock.js
@@ -0,0 +1,27 @@
+const initialStateMock = {
+  characters: {
+    characters: [
+      {
+        id: 1,
+        name: 'Rick Sanchez',
+        status: 'Alive',
+        species: 'Human',
+        type: '',
+        gender: 'Male',
+        origin: {
+          name: 'Earth (C-137)',
+          url: 'https://rickandmortyapi.com/api/location/1',
+        },
+        location: {
+          name: 'Earth (Replacement Dimension)',
+          url: 'https://rickandmortyapi.com/api/location/20',
+        },
+        image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      },
+    ],
+    nameQuery: 'rick',
+    statusQuery: 'Dead',
+  },
+}
+
+export default initialStateMock
--- src/frontend/__mocks__/ProviderMock.js
+++ src/frontend/__mocks__/ProviderMock.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import initialStateMock from '../__mocks__/initialStateMock'
 import rootReducer from '../store/reducers'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, initialStateMock)
 
 const ProviderMock = (props) => <Provider store={store}>{props.children}</Provider>
 
